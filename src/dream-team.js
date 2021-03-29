20add7d1421ea0ee26cb8db5d684f2ca4743c115
GGwpAiushtha <ggwpaiushtha@gmail.com>
2021-03-30 00:11:15 +0300

    feat: Dream team solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!members) { return false }
+  if (typeof members != "object") { return false }
+  if (members[0] == undefined && members[1] == undefined && members[2] == undefined) { return false }
+  let teamName = ""
+  for (i = 0; i < members.length; i++) {
+    if (typeof members[i] == "string") {
+      for (j = 0; j < members[i].length; j++) {
+        if (members[i][j] != " ") {
+          teamName += members[i][j].toUpperCase()
+          break
+        }
+      }
+    }
+  }
+  let arr = teamName.split("")
+  teamName = arr.sort().join("")
+  return teamName
 };
